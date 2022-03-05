ff55a95c4f02aec69d8ddafa4a6cb18a6006070f
muthuswamy <muthuswamyseni@gmail.com>
2022-03-05 18:41:23 +0530

    first commit in stack.java

---
+++ stack.java
@@ -0,0 +1,21 @@
+import.java.util.stack;
+
+ public class main{
+ public Static void main(String[]args){
+              
+        Stack<String>Stack=newStack<String> ();
+        stack.push("minecraft");
+	stack.push("GTA");
+	stack.push("doom");
+	stack.push("borderlands");
+
+	system.out.println(stack.search("GTA"));
+	system.out.println(stack);
+
+ }
+
+ }
+
+
+		
+            
