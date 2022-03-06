de63506ca0086ffafb3ab67b6259c1d066be2914
muthuswamy <muthuswamyseni@gmail.com>
2022-03-06 13:31:55 +0530

    commited file

--- stack.java
+++ stack.java
@@ -1,14 +1,14 @@
-import.java.util.stack;
+
 
  public class main{
- public Static void main(String[]args){
+ public static void main(String[]args){
               
-        Stack<String>Stack=newStack<String> ();
+        Stack<String>Stack=newStack<String>{
         stack.push("minecraft");
 	stack.push("GTA");
 	stack.push("doom");
 	stack.push("borderlands");
-
+ }
 	system.out.println(stack.search("GTA"));
 	system.out.println(stack);
 
